1da6ff3b0778a2fefae417a9dedc2f923813a6b6
JamesTriton <swagatpanda@Swagats-MacBook-Pro.local>
2018-08-24 19:23:15 +0530

    First commit

---
+++ Pro3.java
@@ -0,0 +1,46 @@
+// Multiple Threads with multiple jobs
+
+public class Pro3 implements Runnable
+{
+	
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(Thread.currentThread().getName());
+		
+		Pro3 p = new Pro3();
+		Thread th1 = new Thread(p);
+		
+		th1.start();
+		
+		Thread th2 = new Thread(new Demo());
+		th2.start();
+	}
+
+	@Override
+	public void run()  // Only one run method can be present inside a class
+	{
+		System.out.println("Logic of fact() executed by : "+ Thread.currentThread().getName());
+		
+		System.out.println(fact(15));
+	}
+
+	private int fact(int i) 
+	{
+		int result = 1;
+		
+		while(i != 0)
+		{
+			result = result * i;
+			i -= 1;
+		}
+		
+		return result;
+
+	}
+
+	
+}
+
+
+// Check Demo.java as it is the continuation of this program. 
